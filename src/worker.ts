c0811fedb6ebb139134b14fdc5e8e11f293bf2ad
Will Dereham <12406349+WillDereham@users.noreply.github.com>
2023-06-10 16:21:07 +0100

    Compare secrets securely

--- src/worker.ts
+++ src/worker.ts
@@ -1,11 +1,19 @@
 import router from './router'
 
+const encoder = new TextEncoder()
+
+function compareSecrets(userSecret: string, secretKey: string) {
+  if (userSecret.length !== secretKey.length) return false
+  return crypto.subtle.timingSafeEqual(encoder.encode(userSecret), encoder.encode(secretKey))
+}
+
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     const url = new URL(request.url)
     const secret = url.searchParams.get('secret')
-    if (secret != env.SECRET_KEY) return new Response('401 Unauthorized', { status: 401 })
-
+    if (!compareSecrets(secret, env.SECRET_KEY)) {
+      return new Response('401 Unauthorized', { status: 401 })
+    }
     return router.handle(request, env, ctx)
 
     return new Response('404 Not Found', { status: 404 })
